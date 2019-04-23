e4d8df4856912583f51d1f2c7cf3129a3ed61d50
Krzysztof Moczała <k.moczala@nomtek.com>
2019-04-23 08:34:53 +0200

    Add a missing import statement

--- platform/lumin-runtime/types/view-mode.js
+++ platform/lumin-runtime/types/view-mode.js
@@ -2,6 +2,8 @@
  * @exports ViewMode
  * @description Represents the supported text entry modes.
  */ 
+import * as lumin from 'lumin';
+
 export const ViewMode = {
     'full-area': lumin.ViewMode.kFullArea,
     'left-right' : lumin.ViewMode.kLeftRight
