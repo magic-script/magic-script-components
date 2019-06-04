fcefda20d5827ba31af310251df6e065957566f0
Nikolay Grozdanov <ngrozdanov@magicleap.com>
2019-06-04 16:39:38 -0400

    Add HandGestureKeypointName enum

--- platform/lumin-runtime/types/event-data/gesture-input-event-data.js
+++ platform/lumin-runtime/types/event-data/gesture-input-event-data.js
@@ -1,9 +1,81 @@
 // Copyright (c) 2019 Magic Leap, Inc. All Rights Reserved
 
-import { InputEventData } from './input-event-data.js';
+import { ControlTouchPadInputEventData } from './control-touch-pad-input-event-data.js';
+import { GestureType } from '../gesture-type.js';
+import { GestureDirection } from '../gesture-direction.js';
+import { HandGestureKeypointName } from '../hand-gesture-keypoint-name.js';
 
-export class GestureInputEventData extends InputEventData {
-    getGesture() {
+import { extractor } from '../../utilities/extractor.js';
 
+export class GestureInputEventData extends ControlTouchPadInputEventData {
+    get Gesture() {
+        return extractor.getKeyByValue(GestureType, this._nativeEvent.getGesture());
+    }
+
+    get GestureAngle() {
+        return this._nativeEvent.getGestureAngle();
+    }
+
+    get GestureDirection() {
+        return extractor.getKeyByValue(GestureDirection, this._nativeEvent.getGestureDirection());
+    }
+
+    get GestureDistance() {
+        return this._nativeEvent.getGestureDistance();
+    }
+
+    get GestureFingerGap() {
+        return this._nativeEvent.getGestureFingerGap();
+    }
+
+    get GestureForceValue() {
+        return this._nativeEvent.getGestureForceValue();
+    }
+
+    get GestureLocation() {
+        return this._nativeEvent.getGestureLocation();
+    }
+
+    get GestureRadius() {
+        return this._nativeEvent.getGestureRadius();
+    }
+
+    get GestureSpeed() {
+        return this._nativeEvent.getGestureSpeed();
+    }
+
+    get HandGestureConfidence() {
+        return this._nativeEvent.getHandGestureConfidence();
+    }
+
+    get HandGestureIndex() {
+        return this._nativeEvent.getHandGestureIndex();
+    }
+
+    getHandGestureKeypoint(KeypointName) {
+        // validate KeypointName against HandGestureKeypointName
+        return this._nativeEvent.getHandGestureKeypoint();
+    }
+
+    get HandGestureKeyPointCount() {
+        return this._nativeEvent.getHandGestureKeyPointCount();
+    }
+
+    get HandGestureKeyPoseConfidence(gesture) {
+        // validate gesture against GestureType
+        return this._nativeEvent.getHandGestureKeyPoseConfidence(gesture);
+    }
+
+    get HandGestureLocation() {
+        return this._nativeEvent.getHandGestureLocation();
+    }
+
+    get isHandGestureKeypointRecognized() {
+        return this._nativeEvent.isHandGestureKeypointRecognized();
+    }
+
+    get isMultiTouch() {
+        return this._nativeEvent.isMultiTouch();
     }
 }
+
---
+++ platform/lumin-runtime/types/hand-gesture-keypoint-name.js
@@ -0,0 +1,35 @@
+// Copyright (c) 2019 Magic Leap, Inc. All Rights Reserved
+
+import { input } from 'lumin';
+
+/**
+ * @exports HandGestureKeypointName
+ * @description Represents Magic-Script - Lumin Runtime HandGestureKeypointName mapping.
+ */
+export const HandGestureKeypointName = {
+    'hand-center': input.HandGestureKeypointName.HAND_CENTER,
+    'index-finger-base': input.HandGestureKeypointName.INDEX_FINGER_BASE,
+    'index-finger-first-joint': input.HandGestureKeypointName.INDEX_FINGER_FIRST_JOINT,
+    'index-finger-second-joint': input.HandGestureKeypointName.INDEX_FINGER_SECOND_JOINT,
+    'index-finger-tip': input.HandGestureKeypointName.INDEX_FINGER_TIP,
+    'middle-finger-base': input.HandGestureKeypointName.MIDDLE_FINGER_BASE,
+    'middle-finger-first-joint': input.HandGestureKeypointName.MIDDLE_FINGER_FIRST_JOINT,
+    'middle-finger-second-joint': input.HandGestureKeypointName.MIDDLE_FINGER_SECOND_JOINT,
+    'middle-finger-tip': input.HandGestureKeypointName.MIDDLE_FINGER_TIP,
+    'none': input.HandGestureKeypointName.NONE,
+    'pinky-finger-base': input.HandGestureKeypointName.PINKY_FINGER_BASE,
+    'pinky-finger-first-joint': input.HandGestureKeypointName.PINKY_FINGER_FIRST_JOINT,
+    'pinky-finger-second-joint': input.HandGestureKeypointName.PINKY_FINGER_SECOND_JOINT,
+    'pinky-finger-tip': input.HandGestureKeypointName.PINKY_FINGER_TIP,
+    'ring-finger-base': input.HandGestureKeypointName.RING_FINGER_BASE,
+    'ring-finger-first-joint': input.HandGestureKeypointName.RING_FINGER_FIRST_JOINT,
+    'ring-finger-second-joint': input.HandGestureKeypointName.RING_FINGER_SECOND_JOINT,
+    'ring-finger-tip': input.HandGestureKeypointName.RING_FINGER_TIP,
+    'thumb-base': input.HandGestureKeypointName.THUMB_BASE,
+    'thumb-carpal-joint': input.HandGestureKeypointName.THUMB_CARPAL_JOINT,
+    'thumb-first-joint': input.HandGestureKeypointName.THUMB_FIRST_JOINT,
+    'thumb-tip': input.HandGestureKeypointName.THUMB_TIP,
+    'wrist-center': input.HandGestureKeypointName.WRIST_CENTER,
+    'wrist-pinky-side': input.HandGestureKeypointName.WRIST_PINKY_SIDE,
+    'wrist-thumb-side': input.HandGestureKeypointName.WRIST_THUMB_SIDE
+}
