2fbd49527bd0afee3bb1396d35a1afbcdc8ea837
Konrad Piascik <kpiascik@yahoo.com>
2019-09-05 12:22:14 -0400

    Revert "Add ServerEvent.ServerEventType property"

--- src/platform/lumin-runtime/types/event-data/server-event.js
+++ src/platform/lumin-runtime/types/event-data/server-event.js
@@ -1,8 +1,6 @@
 // Copyright (c) 2019 Magic Leap, Inc. All Rights Reserved
 import { ServerEvent as _ServerEvent } from 'lumin';
 import { EventData } from './event-data.js';
-import { ServerEventType } from '../server-event-type.js';
-import { extractor } from '../../utilities/extractor.js';
 
 export class ServerEvent extends EventData {
     constructor(nativeEvent) {
@@ -13,12 +11,13 @@ export class ServerEvent extends EventData {
         ]);
     }
 
+    // get PrismId() {
+    //     return this._nativeEvent.getPrismId();
+    // }
+
     get isInputEvent() {
         return this._nativeEvent.isInputEventType();
     }
-
-    get ServerEventType() {
-        return extractor.getKeyByValue(ServerEventType, this._nativeEvent.getServerEventType());
 
     // Returns a string representation of this event for debug purposes.
     toString() {
