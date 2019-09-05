9ce56755f8fd014e67d7bba9996bf301514b01f2
Leonid Zolotarev <lzolotarev@magicleap.com>
2019-09-05 11:34:07 -0400

    Deploy website version based on 3e0ccc7957e7757a44b19cc05b61f00ef3086ed9

---
+++ metadata---docs-types-texture-typee-4-d-468.e1a7f817e5859e0b4625.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[89],{121:function(e){e.exports=JSON.parse('{"id":"types/textureType","title":"Texture Type","description":"# Texture Type options:","source":"@site/../docs/types/TextureType.md","permalink":"/docs/types/textureType","sidebar":"docs","category":"Types","previous":"types/side","previous_title":"Side"}')}}]);
