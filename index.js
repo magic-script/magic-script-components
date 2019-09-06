fd61318474956dc26be6c4dda9b4a4dad3876d88
panwrona <mariusz.brona@gmail.com>
2019-09-06 16:10:59 +0200

    remove unnecessary files from src

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-import configuration from './src/configuration.js';
-
-export default {
-    _init() {
-        this._nativeFactory = new configuration.nativeFactory(configuration.nativeMapping);
-    },
-
-    bootstrap(app) {
-        this._init();
-
-        this._app = this._nativeFactory.createApp(app);
-    },
-
-    getContainer(nodeName) {
-        return this._app.getContainer(nodeName);
-    }
-}
 
 export {
     Audio,
