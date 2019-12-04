4a18adef5473213deee83f42e50c9768327fb3d6
Tony Cuadra <tcuadra@magicleap.com>
2019-12-04 18:16:20 -0500

    Regenerate TypeScript declarations with missing props

--- index.d.ts
+++ index.d.ts
@@ -74,6 +74,9 @@ declare module "magic-script-components" {
     weight?: FontWeight;
     boundsSize?: { boundsSize?: vec2; wrap?: boolean; };
     fontParameters?: { style?: FontStyle; weight?: FontWeight; fontSize?: number; tracking?: number; allCaps?: boolean; };
+    fontDescription?: FontDescription;
+    filePath?: string;
+    absolutePath?: boolean;
   }
 
   const Text: React.FC<TextProps>;
@@ -125,6 +128,9 @@ declare module "magic-script-components" {
     fontParameters?: { style?: FontStyle; weight?: FontWeight; fontSize?: number; tracking?: number; allCaps?: boolean; };
     width?: number;
     height?: number;
+    fontDescription?: FontDescription;
+    filePath?: string;
+    absolutePath?: boolean;
   }
 
   const TextEdit: React.FC<TextEditProps>;
@@ -201,6 +207,11 @@ declare module "magic-script-components" {
     renderResource?: number;
     width?: number;
     height?: number;
+    icon?: SystemIcon;
+    filePath?: string;
+    resourceId?: bigint;
+    absolutePath?: boolean;
+    useFrame?: boolean;
   }
 
   const Image: React.FC<ImageProps>;
@@ -374,6 +385,11 @@ declare module "magic-script-components" {
     gravityWellProperties?: { shape?: { size?: vec2; offset?: vec3; roundness?: number; }; snap?: GravityWellSnap; internalSnap?: boolean; };
     size?: vec2;
     value?: number;
+    type?: LoadingSpinnerType;
+    id?: bigint;
+    path?: string;
+    height?: number;
+    determinate?: boolean;
   }
 
   const Spinner: React.FC<SpinnerProps>;
@@ -593,6 +609,8 @@ declare module "magic-script-components" {
   const DropdownList: React.FC<DropdownListProps>;
 
   interface DropdownListItemProps {
+    label?: string;
+    id?: number;
   }
 
   const DropdownListItem: React.FC<DropdownListItemProps>;
@@ -627,6 +645,8 @@ declare module "magic-script-components" {
     textColor?: vec4;
     textSize?: number;
     on?: boolean;
+    height?: number;
+    type?: ToggleType;
   }
 
   const Toggle: React.FC<ToggleProps>;
@@ -729,6 +749,7 @@ declare module "magic-script-components" {
     text?: string;
     textColor?: vec4;
     textSize?: number;
+    type?: EclipseLabelType;
   }
 
   const Tab: React.FC<TabProps>;
@@ -766,6 +787,11 @@ declare module "magic-script-components" {
     confirmText?: string;
     confirmIcon?: SystemIcon;
     expireTime?: number;
+    message?: string;
+    title?: string;
+    type?: DialogType;
+    layout?: DialogLayout;
+    scrolling?: boolean;
   }
 
   const Dialog: React.FC<DialogProps>;
@@ -877,6 +903,7 @@ declare module "magic-script-components" {
     iconIdleAnimation?: string;
     iconHoverPosition?: vec3;
     labelDisplayMode?: LabelDisplayMode;
+    iconSize?: PortalIconSize;
   }
 
   const PortalIcon: React.FC<PortalIconProps>;
@@ -907,6 +934,7 @@ declare module "magic-script-components" {
     eventSoundId?: { soundEvent?: SoundEvent; soundName?: string; };
     gravityWellProperties?: { shape?: { size?: vec2; offset?: vec3; roundness?: number; }; snap?: GravityWellSnap; internalSnap?: boolean; };
     color?: vec4;
+    height?: number;
   }
 
   const ColorPicker: React.FC<ColorPickerProps>;
@@ -939,6 +967,9 @@ declare module "magic-script-components" {
     color?: vec4;
     time?: string;
     showHint?: boolean;
+    label?: string;
+    labelSide?: Side;
+    defaultTime?: string;
   }
 
   const TimePicker: React.FC<TimePickerProps>;
@@ -971,6 +1002,11 @@ declare module "magic-script-components" {
     color?: vec4;
     date?: string;
     showHint?: boolean;
+    label?: string;
+    labelSide?: Side;
+    defaultDate?: string;
+    yearMin?: number;
+    yearMax?: number;
   }
 
   const DatePicker: React.FC<DatePickerProps>;
@@ -1000,6 +1036,7 @@ declare module "magic-script-components" {
     gravityWellEnabled?: boolean;
     eventSoundId?: { soundEvent?: SoundEvent; soundName?: string; };
     gravityWellProperties?: { shape?: { size?: vec2; offset?: vec3; roundness?: number; }; snap?: GravityWellSnap; internalSnap?: boolean; };
+    radius?: number;
   }
 
   const CircleConfirmation: React.FC<CircleConfirmationProps>;
@@ -1065,6 +1102,10 @@ declare module "magic-script-components" {
     modelResourceId?: number;
     animation?: { resourceId?: number; name?: string; paused?: boolean; loops?: number; };
     texture?: { textureId?: number; textureSlot?: string; materialName?: string; };
+    modelPath?: string;
+    materialPath?: string;
+    importScale?: number;
+    texturePaths?: number[];
   }
 
   const Model: React.FC<ModelProps>;
@@ -1108,6 +1149,7 @@ declare module "magic-script-components" {
     texCoords?: vec4;
     viewMode?: ViewMode;
     size?: vec2;
+    subTexture?: string | number;
   }
 
   const Quad: React.FC<QuadProps>;
@@ -1193,6 +1235,13 @@ declare module "magic-script-components" {
     spatialSoundPosition?: { channel?: number; channelPosition?: vec3; };
     spatialSoundRadiation?: { channel?: number; innerAngle?: number; outerAngle?: number; outerGain?: number; outerGainHf?: number; };
     spatialSoundRoomSendLevels?: { channel?: number; gain?: number; gainHf?: number; gainLf?: number; gainMf?: number; };
+    fileName?: string;
+    loadFile?: boolean;
+    absolutePath?: boolean;
+    descriptor?: number;
+    basePath?: string;
+    autoDestroy?: boolean;
+    dynamicDecode?: boolean;
   }
 
   const Audio: React.FC<AudioProps>;
@@ -1295,6 +1344,20 @@ declare module "magic-script-components" {
 
   type FontWeight = 'extra-light' | 'light' | 'regular' | 'medium' | 'bold' | 'extra-bold';
 
+  type FontDescription = {
+    advanceDirection: AdvanceDirection,
+    flowDirection: FlowDirection,
+    tileSize: number,
+    quality?: Quality,
+    minAlpha?: number
+  };
+
+  type AdvanceDirection = 'down' | 'left' | 'right' | 'up';
+
+  type FlowDirection = 'above' | 'below' | 'left' | 'right';
+
+  type Quality = 'enh1' | 'enh1-aa' | 'enh1-fast' | 'enh2' | 'enh3' | 'exper' | 'fast' | 'std' | 'std-0';
+
   type CursorEdgeScrollMode = 'always' | 'auto' | 'never';
 
   type TextEntryMode = 'email' | 'none' | 'normal' | 'numeric' | 'url';
@@ -1305,19 +1368,31 @@ declare module "magic-script-components" {
 
   type ScrollDirection = 'horizontal' | 'vertical';
 
+  type LoadingSpinnerType = 'sprite-animation' | 'particle-package';
+
+  type ToggleType = 'default' | 'checkbox' | 'radio';
+
   type PanelCursorTransitionType = 'center' | 'closest-edge' | 'initial-position';
 
   type Side = 'bottom' | 'left' | 'right' | 'top';
 
   type PanelEdgeConstraintLevel = 'heavy' | 'impassable' | 'light' | 'manual' | 'medium' | 'none';
 
+  type EclipseLabelType = 'B1' | 'B2' | 'B3' | 'B4' | 'C1' | 'C2' | 'C3' | 'C4' | 'T1' | 'T2' | 'T3' | 'T4' | 'T5' | 'T6' | 'T7';
+
   type EclipseButtonType = 'icon' | 'icon-with-label' | 'text' | 'text-with-icon';
 
   type SystemIcon = 'actions' | 'actions-left' | 'actions-off' | 'actions-right' | 'add' | 'address-book' | 'album-add' | 'album-remove' | 'alphabetical' | 'analytics' | 'arrow-down' | 'arrow-left' | 'arrow-right' | 'arrow-up' | 'auto-placement' | 'backspace' | 'battery' | 'block' | 'block-camera' | 'block-cookie' | 'block-location' | 'block-microphone' | 'bluetooth' | 'bluetooth-off' | 'bookmark' | 'bookmark-add' | 'brightness' | 'calendar' | 'camera' | 'camera-iris' | 'carriage-return' | 'chat' | 'check' | 'check-selection' | 'chevron-down' | 'chevron-left' | 'chevron-right' | 'chevron-up' | 'clipboard' | 'clock' | 'close' | 'closed-caption' | 'cloud' | 'cloud-off' | 'collection' | 'controller' | 'cookie' | 'copy' | 'credit-card' | 'cut' | 'cv-camera-privilege' | 'do-not-disturb' | 'dot' | 'download' | 'download-cloud' | 'dropdown' | 'edit' | 'effects-palette' | 'eject' | 'emoji' | 'enter' | 'exit' | 'extraction' | 'eye-tracking' | 'fast-forward' | 'fast-forward-ten-second' | 'file' | 'filter' | 'flag' | 'flag-china' | 'flag-france' | 'flag-germany' | 'flag-japan' | 'flag-south-korea' | 'flag-uk' | 'flag-usa' | 'follow' | 'frame' | 'game-controller' | 'generic-three-dimensional' | 'grid' | 'hamburger' | 'hand-orientation-left' | 'hand-orientation-right' | 'heart' | 'heart-add' | 'help' | 'history' | 'home' | 'image' | 'info' | 'keyboard' | 'keyboard-capital' | 'keyboard-language' | 'keyboard-letter' | 'keyboard-number' | 'link' | 'list-view' | 'local-area-server' | 'marquee-selection' | 'merge' | 'mesh' | 'microphone' | 'microphone-mute' | 'minimize' | 'mobile' | 'more' | 'move' | 'multiple-selection' | 'music' | 'music-pause' | 'music-play' | 'network-server' | 'next' | 'notification' | 'object-recognition' | 'open-with' | 'origin' | 'passable-world' | 'paste' | 'pause' | 'permission' | 'person' | 'person-add' | 'person-block' | 'person-follow' | 'person-three' | 'person-two' | 'person-un-follow' | 'person-voip' | 'phone' | 'phone-end-call' | 'phone-incoming-call' | 'phone-switch-call' | 'pin' | 'placeholder-bluetooth' | 'play' | 'playlist-repeat-off' | 'playlist-repeat-on' | 'playlist-repeat-song' | 'playlist-shuffle' | 'playlist-shuffle-off' | 'power' | 'previous' | 'privacy' | 'private-view' | 'public-view' | 'rearrange' | 'refresh' | 'replay' | 'report' | 'resize' | 'restore-media' | 'revolver' | 'rewind-ten-second' | 'screenshot' | 'search' | 'security-lock' | 'security-not-secure' | 'security-unlock' | 'send' | 'settings' | 'share' | 'share-screen' | 'shift-disabled' | 'shift-enabled' | 'shift-enabled-permanent' | '6-dof' | 'sleep' | 'slide-show' | 'snooze' | 'space' | 'star' | 'stop' | 'stream' | 'stream-off' | 'summon' | 'tablet' | 'tab-new' | 'tab-new-image' | 'tab-view-all' | 'tag' | 'text' | 'thumbs-down' | 'thumbs-down-left' | 'thumbs-down-right' | 'thumbs-up' | 'thumbs-up-left' | 'thumbs-up-right' | 'top-chart' | 'trash' | 'undo-reply' | 'unknown-device' | 'update' | 'upload' | 'upload-cloud' | 'view' | 'view-source' | 'voice-feedback' | 'voip' | 'volume' | 'volume-mute' | 'warning' | 'web-xr' | 'wifi' | 'wifi-no-bar' | 'wifi-no-signal' | 'wifi-off' | 'wifi-one-bar' | 'wifi-secure' | 'wifi-secure-no-bar' | 'wifi-secure-no-signal' | 'wifi-secure-one-bar' | 'wifi-unknown' | 'window-new' | 'window-pop-up-block' | 'window-view-all' | 'work' | 'workspaces' | 'zoom-in' | 'zoom-out';
 
+  type DialogType = 'custom' | 'timed' | 'no-action' | 'single-action' | 'dual-action';
+
+  type DialogLayout = 'standard' | 'wide';
+
   type WebViewAction = 'back' | 'forward' | 'reload';
 
   type LabelDisplayMode = 'always' | 'hover';
+
+  type PortalIconSize = 'extra-large' | 'large' | 'medium' | 'small' | 'extra-small';
 
   type ShaderType = 'diffuse-normal-spec' | 'pbr' | 'unlit-textured';
 
