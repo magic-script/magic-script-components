c2da21d691b3536ccf63ef1277647208b9fd8046
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2020-02-04 10:42:24 +0100

    Update style of ExpandableSection component.

--- website/src/theme/ExpandableSection/index.js
+++ website/src/theme/ExpandableSection/index.js
@@ -2,24 +2,55 @@
 // Distributed under Apache 2.0 License.
 // See LICENSE file in the project root for full license information.
 
-import React, {fetch, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import useBaseUrl from "@docusaurus/useBaseUrl";
+import classnames from 'classnames';
+import styles from './styles.css';
 
 function ExpandableSection(props) {
-  const {children, title} = props;
+  const {children, title, path} = props;
   const [isExpanded, setExpanded] = useState(props.expanded);
+  const [content, setContent] = useState('');
+  const [contentUrl, setContentUrl] = useState((path !== undefined) ? useBaseUrl(path) : undefined);
 
   const onClick = (e) => {
     setExpanded(isExpanded ? false : true);
   }
 
+  const renderContent = () => {
+    if (!isExpanded) {
+      return null;
+    }
+
+    if (content !== '' && content !== undefined) {
+      return <div dangerouslySetInnerHTML={{__html: content}}></div>;
+    }
+
+    return children;
+  }
+
+  useEffect(() => {
+    if (content === '' && contentUrl !== undefined) {
+      fetch(contentUrl)
+      .then(response => response.text())
+      .then(text => setContent(text))
+    }
+  });
+
   return (
     <div>
       <button
+        className={classnames('button', styles.button, {})}
         onClick={() => onClick()}
       >{title}</button>
+      <h2>
+        <a aria-hidden="true" tabindex="-1" class="anchor" id="common-properties"></a>
+        <a aria-hidden="true" tabindex="-1" class="hash-link" href="#common-properties" title="Direct link to heading">#</a>
+        Common Properties
+      </h2>
       <div role="section">
-        {isExpanded && children}
+        {/* {renderContent()} */}
+        {contentUrl}
       </div>
     </div>
   );
