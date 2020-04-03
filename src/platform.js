346e6502a2be21b7309cca2bc84333a24ace4eac
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2020-04-03 12:03:44 +0200

    Add interface for opening external urls (react-native).

--- src/platform.js
+++ src/platform.js
@@ -3,6 +3,18 @@ class Platform {
         Platform.OS = platform.name();
         Platform.Version = platform.version();
     }
+
+    static setLinking(linking) {
+      Platform.linking = linking;
+    }
+
+    static canOpenUrl(url) {
+      return Platform.linking.canOpenURL(url);
+    }
+
+    static openUrl(url) {
+      return Platform.linking.openURL(url);
+    }
 }
 
 export { Platform };
