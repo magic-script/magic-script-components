6274162b91ed6e30bfba885d766f0b29f612397c
Paweł Leszkiewicz <pleszkiewicz@users.noreply.github.com>
2020-04-06 12:02:32 +0200

    Add assert to Platform linking-related methods. (#287)

--- src/plane-detector.js
+++ src/plane-detector.js
@@ -1,75 +1,75 @@
 class PlaneDetector {
-    static setNativePlaneDetector(planeDetector) {
-        PlaneDetector.nativePlaneDetector = planeDetector
-    }
+  static setNativePlaneDetector(planeDetector) {
+    PlaneDetector.nativePlaneDetector = planeDetector
+  }
 
-    static startDetecting(configuration) {
-        PlaneDetector.assertNativePlaneDetector();
+  static startDetecting(configuration) {
+    PlaneDetector._assertNativePlaneDetector();
 
-        // configuration sample: { planeType: ["horizontal", "vertical"] }
-        PlaneDetector.nativePlaneDetector.startDetecting(configuration);
-    }
+    // configuration sample: { planeType: ["horizontal", "vertical"] }
+    PlaneDetector.nativePlaneDetector.startDetecting(configuration);
+  }
 
-    static stopDetecting(observer) {
-        PlaneDetector.assertNativePlaneDetector();
+  static stopDetecting(observer) {
+    PlaneDetector._assertNativePlaneDetector();
 
-        PlaneDetector.nativePlaneDetector.stopDetecting(observer);
-    }
+    PlaneDetector.nativePlaneDetector.stopDetecting(observer);
+  }
 
-    static getAllPlanes(configuration, callback) {
-        PlaneDetector.assertNativePlaneDetector();
+  static getAllPlanes(configuration, callback) {
+    PlaneDetector._assertNativePlaneDetector();
 
-        // configuration sample: { planeType: ["horizontal", "vertical"] }
-        // callback sample (error, planes) => { }
-        PlaneDetector.nativePlaneDetector.getAllPlanes(configuration, callback);
-    }
+    // configuration sample: { planeType: ["horizontal", "vertical"] }
+    // callback sample (error, planes) => { }
+    PlaneDetector.nativePlaneDetector.getAllPlanes(configuration, callback);
+  }
 
-    static reset() {
-        PlaneDetector.assertNativePlaneDetector();
+  static reset() {
+    PlaneDetector._assertNativePlaneDetector();
 
-        PlaneDetector.nativePlaneDetector.reset();
-    }
+    PlaneDetector.nativePlaneDetector.reset();
+  }
 
-    static requestPlaneCast(configuration, callback) {
-        PlaneDetector.assertNativePlaneDetector();
+  static requestPlaneCast(configuration, callback) {
+    PlaneDetector._assertNativePlaneDetector();
 
-        // configuration sample: { planeType: "vertical", rayCastParameters: {...}] }
-        // callback sample (error, planes) => { }
-        PlaneDetector.nativePlaneDetector.requestPlaneCast(configuration, callback);
-    }
+    // configuration sample: { planeType: "vertical", rayCastParameters: {...}] }
+    // callback sample (error, planes) => { }
+    PlaneDetector.nativePlaneDetector.requestPlaneCast(configuration, callback);
+  }
 
-    // callbacks registration
-    static addOnPlaneDetectedObserver(observer, observerCallback) {
-        PlaneDetector.assertNativePlaneDetector();
+  // callbacks registration
+  static addOnPlaneDetectedObserver(observer, observerCallback) {
+    PlaneDetector._assertNativePlaneDetector();
 
-        // observerCallback sample data: Plane: { normal: [x, y, z], center: [x, y, z], vertices: [[x, y, z]], id: UUID, type: [String] }
-        PlaneDetector.nativePlaneDetector.addOnPlaneDetectedObserver(observer, observerCallback);
-    }
+    // observerCallback sample data: Plane: { normal: [x, y, z], center: [x, y, z], vertices: [[x, y, z]], id: UUID, type: [String] }
+    PlaneDetector.nativePlaneDetector.addOnPlaneDetectedObserver(observer, observerCallback);
+  }
 
-    static addOnPlaneUpdatedObserver(observer, observerCallback) {
-        PlaneDetector.assertNativePlaneDetector();
+  static addOnPlaneUpdatedObserver(observer, observerCallback) {
+    PlaneDetector._assertNativePlaneDetector();
 
-        // observerCallback sample data: Plane: { normal: [x, y, z], center: [x, y, z], vertices: [[x, y, z]], id: UUID, type: [String] }
-        PlaneDetector.nativePlaneDetector.addOnPlaneUpdatedObserver(observer, observerCallback);
-    }
+    // observerCallback sample data: Plane: { normal: [x, y, z], center: [x, y, z], vertices: [[x, y, z]], id: UUID, type: [String] }
+    PlaneDetector.nativePlaneDetector.addOnPlaneUpdatedObserver(observer, observerCallback);
+  }
 
-    static addOnPlaneRemovedObserver(observer, observerCallback) {
-        PlaneDetector.assertNativePlaneDetector();
+  static addOnPlaneRemovedObserver(observer, observerCallback) {
+    PlaneDetector._assertNativePlaneDetector();
 
-        // observerCallback sample data: Plane: { normal: [x, y, z], center: [x, y, z], vertices: [[x, y, z]], id: UUID, type: [String] }
-        PlaneDetector.nativePlaneDetector.addOnPlaneRemovedObserver(observer, observerCallback);
-    }
+    // observerCallback sample data: Plane: { normal: [x, y, z], center: [x, y, z], vertices: [[x, y, z]], id: UUID, type: [String] }
+    PlaneDetector.nativePlaneDetector.addOnPlaneRemovedObserver(observer, observerCallback);
+  }
 
-    static addOnPlaneTappedObserver(observer, observerCallback) {
-        PlaneDetector.assertNativePlaneDetector();
+  static addOnPlaneTappedObserver(observer, observerCallback) {
+    PlaneDetector._assertNativePlaneDetector();
 
-        // observerCallback sample data: Plane: { normal: [x, y, z], center: [x, y, z], vertices: [[x, y, z]], id: UUID, type: [String] }
-        PlaneDetector.nativePlaneDetector.addOnPlaneTappedObserver(observer, observerCallback);
-    }
+    // observerCallback sample data: Plane: { normal: [x, y, z], center: [x, y, z], vertices: [[x, y, z]], id: UUID, type: [String] }
+    PlaneDetector.nativePlaneDetector.addOnPlaneTappedObserver(observer, observerCallback);
+  }
 
-    static assertNativePlaneDetector() {
-        if (typeof PlaneDetector.nativePlaneDetector == 'undefined') { console.error("NativePlaneDetector not set."); }
-    }
+  static _assertNativePlaneDetector() {
+    if (typeof PlaneDetector.nativePlaneDetector === 'undefined') { console.error('NativePlaneDetector not set.'); }
+  }
 }
 
 export { PlaneDetector };
