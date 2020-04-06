95c292310f44b4686e03f625a4b6eaf7a2389fb4
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2020-04-06 11:47:16 +0200

    Add assert to Platform linking-related methods.

--- src/platform.js
+++ src/platform.js
@@ -1,24 +1,30 @@
 class Platform {
-    static setPlatformInformation(platform) {
-        Platform.OS = platform.name();
-        Platform.Version = platform.version();
-    }
+  static setPlatformInformation(platform) {
+    Platform.OS = platform.name();
+    Platform.Version = platform.version();
+  }
 
-    static setLinking(linking) {
-      Platform.linking = linking;
-    }
+  static setLinking(linking) {
+    Platform.linking = linking;
+  }
 
-    static canOpenUrl(url) {
-      try {
-        return Platform.linking.canOpenURL(url);
-      } catch {
-        return new Promise((resolve, reject) => reject({ message: 'Invalid format.' }));
-      }
+  static canOpenUrl(url) {
+    Platform._assertLinking();
+    try {
+      return Platform.linking.canOpenURL(url);
+    } catch {
+      return new Promise((resolve, reject) => reject({ message: 'Invalid format.' }));
     }
+  }
 
-    static openUrl(url) {
-      return Platform.linking.openURL(url);
-    }
+  static openUrl(url) {
+    Platform._assertLinking();
+    return Platform.linking.openURL(url);
+  }
+
+  static _assertLinking() {
+    if (typeof Platform.linking === 'undefined') { console.error('Platform.Linking not set.'); }
+  }
 }
 
 export { Platform };
