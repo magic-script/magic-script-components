ea8d9a8d5315d8ca6e021521028881cb65f3b46e
Paweł Leszkiewicz <pleszkiewicz@users.noreply.github.com>
2020-04-06 06:21:47 +0200

    Add interface for opening external urls (react-native). (#286)
    * Add interface for opening external urls (react-native).
    
    * Update canOpenUrl().

--- src/platform.js
+++ src/platform.js
@@ -3,6 +3,22 @@ class Platform {
         Platform.OS = platform.name();
         Platform.Version = platform.version();
     }
+
+    static setLinking(linking) {
+      Platform.linking = linking;
+    }
+
+    static canOpenUrl(url) {
+      try {
+        return Platform.linking.canOpenURL(url);
+      } catch {
+        return new Promise((resolve, reject) => reject({ message: 'Invalid format.' }));
+      }
+    }
+
+    static openUrl(url) {
+      return Platform.linking.openURL(url);
+    }
 }
 
 export { Platform };
