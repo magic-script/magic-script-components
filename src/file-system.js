fcebc5bb0562aeac75c0edf97dd644a4c6cbf555
panwrona <mariusz.brona@gmail.com>
2020-04-21 12:49:35 +0200

    Add missing move and delete functions

--- src/file-system.js
+++ src/file-system.js
@@ -1,18 +1,26 @@
 class FileSystem {
     static setNativeFileSystem(fileSystem) {
-        FileSystem.nativeFileSystem = fileSystem
+        FileSystem.nativeFileSystem = fileSystem;
     }
 
     static writeFile(path, content, encoding) {
-        return FileSystem.nativeFileSystem.writeFile(path, content, encoding)
+        return FileSystem.nativeFileSystem.writeFile(path, content, encoding);
     }
 
     static readFile(path, encoding) {
-        return FileSystem.nativeFileSystem.readFile(path, encoding)
+        return FileSystem.nativeFileSystem.readFile(path, encoding);
+    }
+
+    static deleteFile(path) {
+        return FileSystem.nativeFileSystem.deleteFile(path);
+    }
+
+    static moveFile(srcPath, dstPath) {
+        return FileSystem.nativeFileSystem.moveFile(srcPath, dstPath);
     }
 
     static __assertFileSystem() {
-        if(typeof FileSystem.nativeFileSystem === 'undefined') { console.error('Native FileSystem not set.') }
+        if(typeof FileSystem.nativeFileSystem === 'undefined') { console.error('Native FileSystem not set.'); }
     }
 }
 
