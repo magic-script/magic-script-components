fa7bdd8c8ff7be6d4863ee8f3c7fb69b748ab4da
panwrona <mariusz.brona@gmail.com>
2020-04-27 16:10:03 +0200

    Add assertions to functions

--- src/file-system.js
+++ src/file-system.js
@@ -4,18 +4,22 @@ class FileSystem {
     }
 
     static writeFile(path, content, encoding) {
+        __assertFileSystem();
         return FileSystem.nativeFileSystem.writeFile(path, content, encoding);
     }
 
     static readFile(path, encoding) {
+        __assertFileSystem();
         return FileSystem.nativeFileSystem.readFile(path, encoding);
     }
 
     static deleteFile(path) {
+        __assertFileSystem();
         return FileSystem.nativeFileSystem.deleteFile(path);
     }
 
     static moveFile(srcPath, dstPath) {
+        __assertFileSystem();
         return FileSystem.nativeFileSystem.moveFile(srcPath, dstPath);
     }
 
