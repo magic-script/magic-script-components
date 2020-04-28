d7c5c5a8feb42a8e0f0ed39ea05fab0aeaa97ff2
panwrona <mariusz.brona@gmail.com>
2020-04-28 18:05:30 +0200

    Fix scoping of function invocation

--- src/file-system.js
+++ src/file-system.js
@@ -4,22 +4,22 @@ class FileSystem {
     }
 
     static writeFile(path, content, encoding) {
-        __assertFileSystem();
+        FileSystem.__assertFileSystem();
         return FileSystem.nativeFileSystem.writeFile(path, content, encoding);
     }
 
     static readFile(path, encoding) {
-        __assertFileSystem();
+        FileSystem.__assertFileSystem();
         return FileSystem.nativeFileSystem.readFile(path, encoding);
     }
 
     static deleteFile(path) {
-        __assertFileSystem();
+        FileSystem.__assertFileSystem();
         return FileSystem.nativeFileSystem.deleteFile(path);
     }
 
     static moveFile(srcPath, dstPath) {
-        __assertFileSystem();
+        FileSystem.__assertFileSystem();
         return FileSystem.nativeFileSystem.moveFile(srcPath, dstPath);
     }
 
