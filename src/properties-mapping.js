867eb28b08badd071720c9b34584a8af33d51e10
Nikolay Grozdanov <ngrozdanov@magicleap.com>
2020-05-06 13:27:38 -0400

    Refactor properties-mapping.js

--- src/properties-mapping.js
+++ src/properties-mapping.js
@@ -1,99 +1,43 @@
+import { componentInternalProperties } from './internal-properties.js';
+import { getPropertyValue } from './property-mappers.js';
 
-function extractValue(properties, key, defaultValue = undefined) {
-  const style = properties.style;
-  if (style !== undefined && style[key] !== undefined) {
-    return style[key];
-  }
+import {
+  mapBoundsSize,
+  mapFontParameters
+} from './property-mappers.js';
 
-  return (properties[key] !== undefined) ? properties[key] : defaultValue;
-}
-
-function mapBoundsSize(publicProperties) {
-  var width = extractValue(publicProperties, 'width');
-  var height = extractValue(publicProperties, 'height');
-  var multiline = extractValue(publicProperties, 'multiline');
-  if (width === undefined && height === undefined && multiline === undefined) {
-    return undefined;
-  }
-
-  if (width === undefined) { width = 0 }
-  if (height === undefined) { height = 0 }
-  if (multiline === undefined) { multiline = false }
-
-  // TODO: What if any of the prop (width, height, multiplie) is undefined? The app on Lumin will crash, won't it?
-  // Should we provide default values in such cases?
-  return {
-    boundsSize: [width, height],
-    wrap: multiline
-  };
-}
-
-function mapFontParameters(publicProperties) {
-  var fontStyle = extractValue(publicProperties, 'fontStyle');
-  var fontWeight = extractValue(publicProperties, 'fontWeight');
-  var fontSize = extractValue(publicProperties, 'fontSize');
-  var allCaps = extractValue(publicProperties, 'allCaps');
-  if (fontStyle === undefined && fontWeight === undefined && fontSize === undefined && allCaps === undefined) {
-    return undefined;
-  }
-
-  // TODO: How to handle default values?
-  if (fontStyle === undefined) { fontStyle = 'normal' }
-  if (fontWeight === undefined) { fontWeight = 'regular' }
-  if (fontSize === undefined) { fontSize = 0.08 }
-  if (allCaps === undefined) { allCaps = false }
-
-  return { fontStyle, fontWeight, fontSize, allCaps };
+function setPropertyMapper (name) {
+  return (properties) => getPropertyValue(properties, name);
 }
 
 // Provides map-function or name for each internalProperty
 const propertyMapper = {
   // TransformNode
-  localPosition: 'position',
-  localRotation: 'rotation',
-  localScale: 'scale',
-  localTransform: 'transform',
+  localPosition: setPropertyMapper('position'),
+  localRotation: (publicProperties) => getPropertyValue(publicProperties, 'rotation'),
+  localScale: (publicProperties) => getPropertyValue(publicProperties, 'scale'),
+  localTransform: (publicProperties) => getPropertyValue(publicProperties, 'transform'),
   // UiNode
-  alignment: 'anchorPoint',
+  alignment: (publicProperties) => getPropertyValue(publicProperties, 'anchorPoint'),
   // Text
   boundsSize: mapBoundsSize,
-  charSpacing: 'letterSpacing',
+  charSpacing: (publicProperties) => getPropertyValue(publicProperties, 'letterSpacing'),
   fontParameters: mapFontParameters,
-  textAlignment: 'textAlign',
+  textAlignment: (publicProperties) => getPropertyValue(publicProperties, 'textAlign'),
   // TODO: How to handle cases when the same property (fontSize) is used in multiple places?
   // (e.g. as textSize and in fontParameters.fontSize)
-  textSize: 'fontSize',
-}
-
-// Property name list for the base components
-const nodeInternalProperties = ['debug', 'children', 'name', 'visible', 'skipRaycast'];
-const transformNodeInternalProperties = [...nodeInternalProperties, 'localPosition', 'localRotation', 'localScale', 'localTransform', 'anchorPosition'];
-const renderNodeInternalProperties = [...transformNodeInternalProperties, 'color', 'isOpaque'];
-const uiNodeInternalProperties = [...transformNodeInternalProperties, 'alignment', 'enabled', 'hideBounds'];
-const uiLayoutInternalProperties = [...uiNodeInternalProperties, 'width', 'height'];
-
-// Provides list of the internal property names per component
-const componentInternalProperties = {
-  Text: [...uiNodeInternalProperties, 'textAlignment', 'textColor', 'letterSpacing', 'lineSpacing', 'fontParameters', 'boundsSize'],
-  Button: [...uiNodeInternalProperties, 'iconSize', 'iconColor']
+  textSize: (publicProperties) => getPropertyValue(publicProperties, 'fontSize'),
 }
 
 // Generic function for assembling internal properties from the public properties
 function mapComponentProperties(internalPropertiesNames, publicProperties) {
   const internalProperties = {};
+  
   internalPropertiesNames.forEach(name => {
-    const mapperValue = propertyMapper[name];
-    var value = undefined;
-    if (typeof mapperValue === 'function') {
-      value = mapperValue(publicProperties);
-    } else if ((typeof mapperValue === 'string')) {
-      value = extractValue(publicProperties, mapperValue);
-    } else {
-      value = extractValue(publicProperties, name);
-      if (value !== undefined) {
-        console.log('Unchanged property: ', name, value);
-      }
-    }
+    const mapper = propertyMapper[name];
+    const value = (typeof mapper === 'function')
+      ? mapper(publicProperties)
+      : getPropertyValue(publicProperties, name);
 
     if (value !== undefined) {
       internalProperties[name] = value
@@ -101,16 +45,6 @@ function mapComponentProperties(internalPropertiesNames, publicProperties) {
   });
 
   return internalProperties;
-
-  // TODO: Should we allow all public props to pass through if they do not match any of the internal prop?
-  // const customPublicProperties = Object.keys(publicProperties)
-  //   .filter(name => internalPropertiesNames[name] === undefined)
-  //   .reduce((obj, key) => {
-  //     obj[key] = publicProperties[key];
-  //     return obj;
-  //   }, {});
-
-  // return { ...customPublicProperties, ...internalProperties };
 }
 
 // Custom mapping function for Button component
---
+++ src/property-mappers.js
@@ -0,0 +1,42 @@
+function getPropertyValue(properties, name, defaultValue = undefined) {
+    const style = properties.style;
+    if (style !== undefined && style[name] !== undefined) {
+      return style[name];
+    }
+  
+    return (properties[name] !== undefined) ? properties[name] : defaultValue;
+}
+
+function allUndefined (...properties) {
+    return properties.every(value => value === undefined);
+}
+
+function mapBoundsSize(publicProperties) {
+    const width = getPropertyValue(publicProperties, 'width');
+    const height = getPropertyValue(publicProperties, 'height');
+    const multiline = getPropertyValue(publicProperties, 'multiline');
+  
+    return allUndefined(width, height, multiline) 
+        ? undefined
+        : {
+            boundsSize: [width, height],
+            wrap: multiline
+        };
+}
+
+function mapFontParameters(publicProperties) {
+    const fontStyle = getPropertyValue(publicProperties, 'fontStyle');
+    const fontWeight = getPropertyValue(publicProperties, 'fontWeight');
+    const fontSize = getPropertyValue(publicProperties, 'fontSize');
+    const allCaps = getPropertyValue(publicProperties, 'allCaps');
+
+    return allUndefined(fontStyle, fontWeight, fontSize, allCaps) 
+        ? undefined
+        : { fontStyle, fontWeight, fontSize, allCaps };
+}
+
+export {
+    getPropertyValue,
+    mapBoundsSize,
+    mapFontParameters
+}
