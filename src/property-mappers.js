025a5d0b71befe8a00ae04fa0b9a9634f1e47154
grozdanov <nikolay.grozdanov@gmail.com>
2020-05-11 15:14:31 -0400

    WIP: Add the renamed properties to properties-mapping.js

--- src/property-mappers.js
+++ src/property-mappers.js
@@ -1,4 +1,4 @@
-function getPropertyValue(properties, name, defaultValue = undefined) {
+function getPropertyValue (properties, name, defaultValue = undefined) {
     const style = properties.style;
     if (style !== undefined && style[name] !== undefined) {
       return style[name];
@@ -11,7 +11,7 @@ function allUndefined (...properties) {
     return properties.every(value => value === undefined);
 }
 
-function mapBoundsSize(publicProperties) {
+function mapBoundsSize (publicProperties) {
     const width = getPropertyValue(publicProperties, 'width');
     const height = getPropertyValue(publicProperties, 'height');
     const multiline = getPropertyValue(publicProperties, 'multiline');
@@ -24,7 +24,7 @@ function mapBoundsSize(publicProperties) {
         };
 }
 
-function mapFontParameters(publicProperties) {
+function mapFontParameters (publicProperties) {
     const fontStyle = getPropertyValue(publicProperties, 'fontStyle');
     const fontWeight = getPropertyValue(publicProperties, 'fontWeight');
     const fontSize = getPropertyValue(publicProperties, 'fontSize');
@@ -35,8 +35,26 @@ function mapFontParameters(publicProperties) {
         : { fontStyle, fontWeight, fontSize, allCaps };
 }
 
+function mapSize (publicProperties) {
+    const width = getPropertyValue(publicProperties, 'width', 0);
+    const height = getPropertyValue(publicProperties, 'height', 0);
+
+    return { width, height };
+}
+
+function mapProgressColor (publicProperties) {
+    const beginColor = getPropertyValue(publicProperties, 'beginColor');
+    const endColor = getPropertyValue(publicProperties, 'endColor');
+
+    return allUndefined(beginColor, endColor)
+        ? undefined
+        : { beginColor, endColor };
+}
+
 export {
     getPropertyValue,
     mapBoundsSize,
-    mapFontParameters
+    mapFontParameters,
+    mapSize,
+    mapProgressColor
 }
