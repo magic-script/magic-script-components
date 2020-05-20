efdf48ce033a7c19682457e338782ef452dd1743
Sebastian Paluchiewicz <sebastian.paluchiewicz@sofomo.com>
2020-05-20 12:12:52 +0200

    AudioNode updated.

--- src/components/util/type-validators.js
+++ src/components/util/type-validators.js
@@ -1,16 +1,38 @@
-
 const TypeValidator = {
-  arrayOf: (length, type = 'number') => {
+  arrayOf: (length, type = "number") => {
     return (propValue, key, componentName, location, propFullName) => {
       if (propValue.length !== length) {
-        return new Error(`Invalid number of elements (${propValue.length}) of array '${propFullName}' supplied to '${componentName}', expected ${length} elements.`);
+        return new Error(
+          `Invalid number of elements (${propValue.length}) of array '${propFullName}' supplied to '${componentName}', expected ${length} elements.`
+        );
       }
 
-      if (typeof(propValue[key]) !== type) {
-        return new Error(`Invalid prop '${propFullName}' of type '${typeof(propValue[key])}' supplied to '${componentName}', expected a '${type}'.`);
+      if (typeof propValue[key] !== type) {
+        return new Error(
+          `Invalid prop '${propFullName}' of type '${typeof propValue[
+            key
+          ]}' supplied to '${componentName}', expected a '${type}'.`
+        );
       }
-    }
+    };
+  },
+  range: (min, max, type = 'number') => {
+    return (propValue, key, componentName) => {
+      if (propValue[key] < min || propValue[key] > max) {
+        return new Error(
+          `Invalid value (${propValue[key]}) of property '${key}' supplied to '${componentName}', expected value in range [${min}..${max}].`
+        );
+      }
+
+      if (typeof propValue[key] !== type) {
+        return new Error(
+          `Invalid prop '${key}' of type '${typeof propValue[
+            key
+          ]}' supplied to '${componentName}', expected a '${type}'.`
+        );
+      }
+    };
   },
 };
 
-export { TypeValidator }
+export { TypeValidator };
