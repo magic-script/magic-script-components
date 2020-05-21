954726aeb55234eed8069d77c3496942ec3a4347
Sebastian Paluchiewicz <sebastian.paluchiewicz@sofomo.com>
2020-05-21 13:26:38 +0200

    VideoNode updated.

--- src/components/video.js
+++ src/components/video.js
@@ -1,19 +1,49 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+
 import { TypeValidator } from './util/type-validators.js';
 import { UiNode } from './types/ui-node.js';
 
 import mapProperties from '../properties-mapping.js';
+import { VideoAction, VideoViewMode } from './types/enums.js';
 
 export function Video (props) {
   return React.createElement('video', mapProperties('Video', props));
 }
 
-// TODO:
-// Video.propTypes = {
-//     ...UiNode.PropTypes,
-// }
+Video.propTypes = {
+  ...UiNode.PropTypes,
 
-// Video.defaultProps = {
-//     ...UiNode.DefaultProps,
-// }
+  path: PropTypes.string.isRequired,
+  looping: PropTypes.bool,
+  subtitlePath: PropTypes.string,
+  volume: TypeValidator.range(0.0, 1.0),
+  seekTo: PropTypes.number,
+  action: PropTypes.oneOf([
+    VideoAction.pause,
+    VideoAction.start,
+    VideoAction.stop
+  ]),
+
+  viewMode: PropTypes.oneOf([
+    VideoViewMode.fullArea,
+    VideoViewMode.leftRight
+  ]),
+  width: PropTypes.number,
+  height: PropTypes.number
+};
+
+Video.defaultProps = {
+  ...UiNode.DefaultProps,
+
+  path: '',
+  looping: false,
+  subtitlePath: '',
+  volume: 1.0,
+  seekTo: 0.0,
+  action: VideoAction.stop,
+
+  viewMode: VideoViewMode.fullArea,
+  width: 0.0,
+  height: 0.0
+};
