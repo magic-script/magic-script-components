03625f59a0cdce3e9f31ff10362bbcabb1f6216d
Sebastian Paluchiewicz <sebastian.paluchiewicz@sofomo.com>
2020-05-26 09:06:03 +0200

    Rect layout updated.

--- src/components/rect-layout.js
+++ src/components/rect-layout.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { TypeValidator } from './util/type-validators.js';
-import { UiNode } from './types/ui-node.js';
+import React from "react";
+import PropTypes from "prop-types";
+import { TypeValidator } from "./util/type-validators.js";
+import { UiNode } from "./types/ui-node.js";
 
-import mapProperties from '../properties-mapping.js';
+import mapProperties from "../properties-mapping.js";
+import { Alignment } from "./types/enums.js";
 
-export function RectLayout (props) {
-  return React.createElement('rectLayout', mapProperties('RectLayout', props));
+export function RectLayout(props) {
+  return React.createElement("rectLayout", mapProperties("RectLayout", props));
 }
 
-// TODO:
-// RectLayout.propTypes = {
-//     ...UiNode.PropTypes,
-// }
+RectLayout.propTypes = {
+  ...UiNode.PropTypes,
 
-// RectLayout.defaultProps = {
-//     ...UiNode.DefaultProps,
-// }
+  alignment: PropTypes.oneOf(Object.values(Alignment)),
+  padding: PropTypes.arrayOf(TypeValidator.arrayOf(4, "number"))
+};
+
+RectLayout.defaultProps = {
+  ...UiNode.DefaultProps,
+
+  alignment: Alignment.topLeft,
+  padding: [0, 0, 0, 0]
+};
