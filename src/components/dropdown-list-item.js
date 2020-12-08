9ed67bcab4971904e33c2c95a27b1c0292dc88ae
Pawel Leszkiewicz <p.leszkiewicz@nomtek.com>
2020-12-08 11:48:08 +0100

    Update DropdownListItem component.

--- src/components/dropdown-list-item.js
+++ src/components/dropdown-list-item.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { UiNode } from './types/ui-node.js';
 import mapProperties from '../properties-mapping.js';
-import { ColorProp } from './common-props';
 
 export function DropdownListItem(props) {
   return React.createElement('dropdownListItem', mapProperties('DropdownListItem', props));
 }
 
 DropdownListItem.propTypes = {
-  ...UiNode.PropTypes,
-
   // Properties
   id: PropTypes.number,
   label: PropTypes.string
 };
 
 DropdownListItem.defaultProps = {
-  ...UiNode.DefaultProps,
-
   id: 0,
-  label: '',
-  selected: false
+  label: ''
 };
